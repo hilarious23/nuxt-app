e2f6d006aa7bcd2edac0537c8eef8231fc890c8e
hilarious23 <s.positex11@gmail.com>
2018-09-07 13:04:46 +0900

    27 create sections, pages, font

---
+++ pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about-page">
+        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam similique facilis placeat nihil nobis eum, numquam cupiditate maxime minima molestiae sed unde voluptatem dolorem illo corporis? Quia cum a numquam?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis optio cumque consequatur quo reiciendis nobis, accusantium quaerat tenetur ullam repellat explicabo fuga ipsum doloremque itaque. Rem ipsa placeat consectetur impedit.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis optio cumque consequatur quo reiciendis nobis, accusantium quaerat tenetur ullam repellat explicabo fuga ipsum doloremque itaque. Rem ipsa placeat consectetur impedit.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis optio cumque consequatur quo reiciendis nobis, accusantium quaerat tenetur ullam repellat explicabo fuga ipsum doloremque itaque. Rem ipsa placeat consectetur impedit.</p>
+    </div>
+</template>
+
+<style scoped>
+.about-page {
+    text-align: center;
+    padding: 30px;
+}
+</style>
